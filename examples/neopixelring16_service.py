26eb9dd2ae44433e746841cd66cf4c3061b3043c
colino17 <51090962+colino17@users.noreply.github.com>
2024-02-20 16:23:08 -0400

    Update neopixelring16_service.py

--- examples/neopixelring16_service.py
+++ examples/neopixelring16_service.py
@@ -16,15 +16,12 @@ from typing import Tuple
 from wyoming.asr import Transcript
 from wyoming.event import Event
 from wyoming.satellite import (
-    RunSatellite,
     SatelliteConnected,
     SatelliteDisconnected,
-    StreamingStarted,
-    StreamingStopped,
 )
+from wyoming.tts import Synthesize
 from wyoming.snd import Played
 from wyoming.server import AsyncEventHandler, AsyncServer
-from wyoming.vad import VoiceStarted
 from wyoming.wake import Detection
 
 ###############
@@ -32,14 +29,14 @@ from wyoming.wake import Detection
 ###############
 # This section defines the GPIO pin (GPIO 21) that the Neopixel Ring is attached to. It also defines the number of LEDs in the ring (16).
 pixels = neopixel.NeoPixel(board.D21, 16)
-# These are the various colors (RGB) that can be used throughout. Max brightness white would be "255, 255, 255", however this seems exceedingly bright and also seems to get quite hot if set for more than a few seconds.
-red = 80, 0, 0
-blue = 0, 40, 80
-green = 0, 80, 20
-white = 80, 80, 80
-purple = 80, 0, 80
-yellow = 80, 50, 0
-orange = 80, 10, 0
+# These are the various colors (RGB) that can be used throughout. Max brightness white would be "255, 255, 255", however this seems exceedingly bright and a>
+red = 50, 0, 0
+blue = 0, 20, 50
+green = 10, 50, 0
+white = 50, 50, 50
+purple = 40, 0, 50
+yellow = 50, 20, 0
+orange = 50, 10, 0
 black = 0, 0, 0
 # Variable used for logging purposes.
 _LOGGER = logging.getLogger()
@@ -48,7 +45,7 @@ _LOGGER = logging.getLogger()
 ## MAIN FUNCTION ##
 ###################
 async def main() -> None:
-# This handles the command line arguments that the program can use. In this case there are two possible arguments, "--uri" which is required and determines the server IP and port, and "--debug" which is an optional flag to increase the logging level.
+# This handles the command line arguments that the program can use. In this case there are two possible arguments, "--uri" which is required and determines >
     parser = argparse.ArgumentParser()
     parser.add_argument("--uri", required=True, help="unix:// or tcp://")
     parser.add_argument("--debug", action="store_true", help="Log DEBUG messages")
@@ -65,7 +62,7 @@ async def main() -> None:
         pass
     finally:
         pixels.fill((black))
-        
+
 ###############################
 ## EVENT HANDLER FOR CLIENTS ##
 ###############################
@@ -84,49 +81,48 @@ class LEDsEventHandler(AsyncEventHandler):
         _LOGGER.debug("Client connected: %s", self.client_id)
 
 # This function is used to add a 0.2 second delay.
-    def sleepLED():
-        time.sleep(0.2)
+    def sleepLED(self):
+        time.sleep(0.25)
 # This function can be called to turn off the LEDs.
-    def clearLED():
+    def clearLED(self):
         pixels.fill((black))
 # This function will flash the LEDs twice using whatever color is set as "fc".
-    def flashLED(fc):
+    def flashLED(self, fc):
         for i in range(2):
             pixels.fill((fc))
-            sleepLED()
-            clearLED()
-            sleepLED()
-        await asyncio.sleep(1.0)
+            self.sleepLED()
+            self.clearLED()
+            self.sleepLED()
 # This function sets the LEDs to whatever solid color is set as "sc".
-    def solidLED(sc):
+    def solidLED(self, sc):
         pixels.fill((sc))
 
     async def handle_event(self, event: Event) -> bool:
         _LOGGER.debug(event)
-# This event is triggered when the server is listening for a speech to text request.
-        if StreamingStarted.is_type(event.type):
-           solidLED(blue) 
 # This event is triggered when the wake word is detected.
-        elif Detection.is_type(event.type):
-            flashLED(white)
+        if Detection.is_type(event.type):
+            self.flashLED(white)
 # This event is triggered when the server has successfully transcribed the speech to text request.
         elif Transcript.is_type(event.type):
-            flashLED(green)
-# This event is triggered when the TTS response playback has ended.          
+            self.flashLED(green)
+# This event is triggered when the TTS service is responding.
+        elif Synthesize.is_type(event.type):
+            self.solidLED(blue)
+# This event is triggered when the TTS response playback has ended.
         elif Played.is_type(event.type):
-            clearLED()
+            self.clearLED()
 # This event is triggered when the satellite connects to the main server.
         elif SatelliteConnected.is_type(event.type):
-            flashLED(green)
-            sleepLED()
-            flashLED(green)
+            self.flashLED(green)
+            self.sleepLED()
+            self.flashLED(green)
 # This event is triggered when the satellite is unable to connect to the main server.
         elif SatelliteDisconnected.is_type(event.type):
-            flashLED(orange)
-            sleepLED()
-            flashLED(orange)
-            sleepLED()
-            solidLED(red)
+            self.flashLED(orange)
+            self.sleepLED()
+            self.flashLED(orange)
+            self.sleepLED()
+            self.solidLED(red)
         return True
 
 if __name__ == "__main__":
